d9127b2e25da7b86d3bc30845f71c9d768eccbcb
dim en <dimendroider@gmail.com>
2021-01-29 23:56:44 +0300

    Lesson02 adv make it smart

--- script.js
+++ script.js
@@ -5,32 +5,20 @@
  */
 
  
-//дано число
 let num = 266219;
-// console.log('num: ', num, 'typeof num:', typeof num);
-
-
-let numStr = num.toString();
 console.log('It is given a number:', num);
 
-
-//разбиваем строку по символам на массив
-let arr = numStr.split('');
-// console.log('arr: ', arr, 'typeof arr: ',typeof arr);
+num = num.toString().split('');
 
 
-//произведение элементов массива
-let product = 1;
-for (let i in arr) {
-    product *= +arr[i];
+let result = 1;
+for (let i in num) {
+    result *= +num[i];
 }
-console.log('the product of all digits is', product); 
+console.log('the product of all digits is', result); 
 
 
-//возводим результат в степень 3
-let result = product ** 3;
-// console.log('product to 3 power:', result);
+result **= 3;
 
 
-//выводим в консоль первые 2 цифры 
 console.log(result.toString().substring(0, 2));
