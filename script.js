3816b75f87e97ee0dd8dda9e4e13d9ba7b7cf802
dim en <dimendroider@gmail.com>
2021-01-29 22:52:01 +0300

    Lesson02 adv feat make some comments

--- script.js
+++ script.js
@@ -7,31 +7,30 @@
  
 //дано число
 let num = 266219;
-console.log('num: ', num, 'typeof num:', typeof num);
+// console.log('num: ', num, 'typeof num:', typeof num);
 
 
-// let numStr = num.toString();
-let numStr = '' + num;
-console.log('numStr: ', numStr, 'typeof numStr: ',typeof numStr);
+let numStr = num.toString();
+console.log('It is given a number:', num);
 
 
 //разбиваем строку по символам на массив
 let arr = numStr.split('');
-console.log('arr: ', arr, 'typeof arr: ',typeof arr);
+// console.log('arr: ', arr, 'typeof arr: ',typeof arr);
 
 
 //произведение элементов массива
 let product = 1;
 for (let i in arr) {
     product *= +arr[i];
-    console.log(i, arr[i], product);
 }
+console.log('the product of all digits is', product); 
 
 
 //возводим результат в степень 3
-product = product ** 3;
-console.log('product to 3 power:', product);
+let result = product ** 3;
+// console.log('product to 3 power:', result);
 
 
 //выводим в консоль первые 2 цифры 
-console.log(product.toString().substring(0, 2));
+console.log(result.toString().substring(0, 2));
