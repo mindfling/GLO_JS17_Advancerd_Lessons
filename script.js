b85e6c2d3a564890aea63d2122a4b31512362926
dima <dimendroider@gmail.com>
2021-01-29 22:19:35 +0300

    Lesson02 adv feat solve task

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-'use strict';
-/**
- * Lesson 02
- * Advanced
+'use strict'; 
+/** 
+ *  Lesson 02
+ *  Advanced
  */
+
+ 
+//дано число
+let num = 266219;
+console.log('num: ', num, 'typeof num:', typeof num);
+
+
+// let numStr = num.toString();
+let numStr = '' + num;
+console.log('numStr: ', numStr, 'typeof numStr: ',typeof numStr);
+
+
+//разбиваем строку по символам на массив
+let arr = numStr.split('');
+console.log('arr: ', arr, 'typeof arr: ',typeof arr);
+
+
+//произведение элементов массива
+let product = 1;
+for (let i in arr) {
+    product *= +arr[i];
+    console.log(i, arr[i], product);
+}
+
+
+//возводим результат в степень 3
+product = product ** 3;
+console.log('product to 3 power:', product);
+
+
+//выводим в консоль первые 2 цифры 
+console.log(product.toString().substring(0, 2));
