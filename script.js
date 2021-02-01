012ad288610a408afc5ee5533e20f162222b6c33
Dimka Mind Fling <dimendroider@gmail.com>
2021-02-01 21:33:33 +0300

    Advanced 03 init vars

--- script.js
+++ script.js
@@ -4,3 +4,24 @@
  * Advanced
  */
 
+let lang = 'ru';
+
+const englishWeek = [ 
+    'Monday', 
+    'Tuesday', 
+    'Wednesday', 
+    'Thursday', 
+    'Friday', 
+    'Saturday', 
+    'Sunday'];
+
+const russianWeek = [
+    'понедельник',
+    'вторник',
+    'среда',
+    'четверг',
+    'пятница',
+    'суббота',
+    'воскресенье'
+];
+
